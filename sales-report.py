8b24dfebb2b77ea74ac54df5e8fc90ac21cfa6f4
Jen Dixon <jendixon85@gmail.com>
2016-07-13 16:19:38 +0000

    Changing code to unpack list and add to dictionary.

--- sales-report.py
+++ sales-report.py
@@ -9,28 +9,30 @@ sales_report.py - Generates sales report showing the total number
 
 """
 
-salespeople = []                 # creating empty lists to add the info fr:
-melons_sold = []                 # txt file, salesperson name & # sold
+sales_data = {}
+
+# salespeople = []                 # creating empty lists to add the info fr:
+# melons_sold = []                 # txt file, salesperson name & # sold
 
 f = open("sales-report.txt")     # opens the sales report file
-for line in f:                  
+for line in f:
     line = line.rstrip()         # formatting - removes extra whitespace
-    entries = line.split("|")    # creating a list out of each line in file
-    salesperson = entries[0]     # names the 1st item in line as 'salesperson'
-    melons = int(entries[2])     # names the 3rd item in line as 'melons sold'
+    line = line.split("|")    # creating a list out of each line in file
+    salesperson, order_amount, melons_sold = line
 
+print line
                                  # it doesn't currently use the amount sold.
                                  # It's easy to add, I'll add it to my queue.
 
-    if salesperson in salespeople:         # if the salesperson's name is 
-        position = salespeople.index(salesperson)  # already in the list, add
-        melons_sold[position] += melons          # to # of melons sold.
-    else:                                  
-        salespeople.append(salesperson)    # if salesperson is not already in
-        melons_sold.append(melons)         # the list, add name # # of melons
+#     if salesperson in salespeople:         # if the salesperson's name is
+#         position = salespeople.index(salesperson)  # already in the list, add
+#         melons_sold[position] += melons          # to # of melons sold.
+#     else:
+#         salespeople.append(salesperson)    # if salesperson is not already in
+#         melons_sold.append(melons)         # the list, add name # # of melons
 
 
-for i in range(len(salespeople)):
-    print "{} sold {} melons".format(salespeople[i], melons_sold[i])
+# for i in range(len(salespeople)):
+#     print "{} sold {} melons".format(salespeople[i], melons_sold[i])
 
                                 # prints report of entire list
